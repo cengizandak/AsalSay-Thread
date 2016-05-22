003b1b0d580a1a5945adcc1efbab5b09d3300d51
Yeni <spear_cengiz@hotmail.com>
2016-05-22 20:20:56 +0300

    Project

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(321, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 97);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BAŞLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(597, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 86);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "BİTİR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(133, 160);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(560, 160);
+            this.listBox1.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Araştırılan Sayı : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Threadler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 332);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Data.SqlClient;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+            
+
+        }
+      
+        public static int baslangic = 1;
+        public static int limitt = 0;
+        public int threadno = 0;
+        public int d = 0;
+        public static int bitiss = 0;
+        public static int baslangicc = 0;
+       public  bool donecek_deger = false;
+       public static bool []don = new bool[100000];
+        public int[]  tut = new int[100000];
+        private volatile bool stopRun = false;
+        private void Kaydet(int sayi,int id)
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-RPONHSG\\CENGIZ; Initial Catalog=yazlabb;User Id=cengiz;password=cengiz123;");
+            baglanti.Open();
+            SqlCommand kmt = new SqlCommand("Insert into asalolanlar(asalsayi,id) VALUES('" + sayi + "','" + id + "') ", baglanti);
+            kmt.ExecuteNonQuery();
+            baglanti.Close();
+
+        }
+        private void Basla(double limit,int sayi,int id,int thread_sayisi)
+        {
+
+            int limit_ = 0; ;
+
+            if (threadno == 0)
+            {
+                baslangic = 1;
+                limitt = limitt + (int)limit;
+                limit_ = (int)limit;
+            }
+
+           else if(threadno>0&&threadno<(thread_sayisi-1))
+            {
+                baslangic = baslangic + (int)limit;
+                limitt = limitt +(int) limit;
+                limit_ = (int)limit;
+            }
+            else if (threadno ==(thread_sayisi - 1))
+            {
+                
+                baslangic = baslangic + (int)limit;
+                limitt = limitt + (int)Math.Ceiling(limit);
+                limit_ = (int)Math.Ceiling(limit);
+                Console.WriteLine("girdi limit : " + (int)Math.Ceiling(limit) + " ve " + limitt);
+            }
+
+
+
+
+
+
+
+            int sayac = 1;
+            int[] bolecek = new int[limit_];
+            Console.WriteLine(sayi+" "+limitt + " " + baslangic);
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-RPONHSG\\CENGIZ; Initial Catalog=yazlabb;User Id=cengiz;password=cengiz123;");
+            baglanti.Open();
+
+            SqlCommand kmt = new SqlCommand("Select asalsayi FROM asalolanlar WHERE id<='" + limitt + "'and id>='" + baslangic + "'", baglanti);
+            SqlDataReader rdr = kmt.ExecuteReader();
+            int s = 0;
+            while (rdr.Read() && s < limit_)
+            {
+
+                bolecek[s] = (int)rdr["asalsayi"];
+
+
+                s++;
+            }
+
+            rdr.Close();
+            bitiss = bolecek[limit_ - 1];
+            baslangicc = bolecek[sayac - 1];
+            threadno++;
+
+            while (sayac <= limit_)
+            {
+                donecek_deger = false;
+
+                if (sayi % bolecek[sayac - 1] == 0)
+                {
+
+                    donecek_deger = true;
+                    tut[threadno-1] = bolecek[sayac - 1];
+                    don[threadno-1] = true; break;
+                }
+                else { donecek_deger = false; don[threadno-1] = false; }
+
+                sayac++;
+                }Console.WriteLine("donecek deger " + donecek_deger.ToString());
+
+            
+            baglanti.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int b, c;
+            b = 5;
+            c = 2;
+            double a = (double)7 / 2;
+            Console.WriteLine(Math.Ceiling(a));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            bool deger = false;
+            int asal = 0;
+            int no = 0;
+            backgroundWorker1.CancelAsync();
+           for(int i=0;;i++)
+            {
+                if (don[i] == true) { no = i; deger = true; asal = tut[i]; break; }
+                else {deger = false; }
+                if (threadno< i) break;
+
+            }
+            if (deger == true) { MessageBox.Show("Durdugunuz deger " + d + " Sayı asal değildir  Thread "+ (no+1) +" daki : "+ asal + "e Bölünmektedir"); }
+            else MessageBox.Show("Durdugunuz deger : " + d + " Sayı asaldır");
+            Thread.Sleep(122);
+            Application.Exit();
+
+
+        }
+      
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+          
+            int son_deger = 0;
+            int son_id = 0;
+            int sayac = 0;
+            int kac_tane = 0;
+            double limit = 0;
+            button2.Enabled = true;
+
+            Thread[] yaz_lab;
+            Thread kaydet;
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-RPONHSG\\CENGIZ; Initial Catalog=yazlabb;User Id=cengiz;password=cengiz123;");
+            baglanti.Open();
+            SqlCommand kmt = new SqlCommand("Select TOP 1 * FROM asalolanlar ORDER BY id DESC ", baglanti);
+
+            SqlDataReader rdr = kmt.ExecuteReader();
+            while (rdr.Read())
+            {
+
+                son_deger = (int)rdr["asalsayi"];
+
+                son_id = (int)rdr["id"];
+
+            }
+            rdr.Close();
+            int tavan_deger = (int)Math.Sqrt(son_deger);
+
+            kmt = new SqlCommand("Select *FROM asalolanlar", baglanti);
+            rdr = kmt.ExecuteReader();
+            while (rdr.Read())
+            {
+                if ((int)rdr["asalsayi"] <= tavan_deger)
+                    sayac++;
+            }
+            rdr.Close();
+            if (sayac < 100) kac_tane = 2;
+            else { kac_tane = (int)(sayac / 100) + 1; }
+
+
+            double hesap = sayac / kac_tane;
+
+            for (int j = son_deger + 2; j < 1490000; j = j + 2)
+            {
+
+
+               
+                d = j;
+
+                if (backgroundWorker1.CancellationPending ==false)
+                
+                {
+                    button2.Enabled = true;
+                    limitt = 0;
+                    baslangicc = 0;
+                    bitiss = 0;
+                    baslangic = 1;
+                    threadno = 0;
+                    hesap = 0;
+                    sayac = 0;
+                   
+                    textBox1.Clear();
+                    listBox1.Items.Clear();
+                    textBox1.Text = j.ToString();
+                    donecek_deger = false;
+                    tavan_deger = (int)Math.Sqrt(j);
+                    baglanti.Close();
+                    baglanti.Open();
+                    kmt = new SqlCommand("Select *FROM asalolanlar", baglanti);
+                    rdr = kmt.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        if ((int)rdr["asalsayi"] <= tavan_deger)
+                            sayac++;
+                    }
+                    rdr.Close();
+                    kmt = new SqlCommand("Select TOP 1 * FROM asalolanlar ORDER BY id DESC ", baglanti);
+
+                    rdr = kmt.ExecuteReader();
+                    while (rdr.Read())
+                    {
+
+                        son_id = (int)rdr["id"];
+
+                    }
+                    rdr.Close();
+                    if (sayac < 100) kac_tane = 2;
+                    else { kac_tane = (int)(sayac / 100) + 1; }
+                    yaz_lab = new Thread[kac_tane];
+                    hesap = (double)sayac / kac_tane;
+                    Console.WriteLine("hesap : "+hesap+" kac tane" + kac_tane+"sayac"+sayac);
+                    for (int i = 0; i < kac_tane; i++)
+                    {
+                        button2.Enabled = true;
+                        yaz_lab[i] = new Thread(() => Basla(hesap, j, (son_id + 1), kac_tane));
+                        yaz_lab[i].Start();
+                        yaz_lab[i].Join(1000);
+
+                        listBox1.Items.Add((i + 1) + ".Thread Baslangıc nosu : " + baslangicc + " Bitis nosu :  " + bitiss + " Asal mi ? " + donecek_deger.ToString());
+                        Thread.Sleep(590);
+                       
+                    }
+
+                    bool deger = false;
+                    for(int i=0;;i++)
+                    {
+                if (don[i] == true){ deger = true; break; }
+                else {deger = false; }
+                if (threadno< i) break;
+
+                     }
+                    
+                    if (deger==false) { Console.WriteLine("Kaydedilecek j deger i :" + j + " " + son_id); kaydet = new Thread(() => Kaydet(j, son_id + 1)); kaydet.Start(); kaydet.Join(1000); }
+
+                }
+                else { e.Cancel = true; }
+
+
+                baglanti.Close();
+            }
+          
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    
+
